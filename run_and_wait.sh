60cb07ab32c1443aede2bd43ddfad503b6ef53a6
khurtado <khurtado@nd.edu>
2016-01-04 11:52:39 -0500

    Update run_and_wait.sh

--- run_and_wait.sh
+++ run_and_wait.sh
@@ -10,4 +10,4 @@ CLUSTER_ID=$(condor_submit $1 | tail -n1 | rev | cut -d' ' -f1 | rev)
 
 LOG_FILE=$(condor_q -format '%s\n' UserLog $CLUSTER_ID | tail -n1)
 
-condor_wait $LOG_FILE $CLUSTER_ID
+condor_wait "$LOG_FILE" "$CLUSTER_ID"
